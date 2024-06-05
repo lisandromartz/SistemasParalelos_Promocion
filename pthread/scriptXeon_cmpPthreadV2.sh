0fef11b3eb98447b6aa67ba3131c8cccd9694325
Lisandro Martinez <lisandromartz@gmail.com>
2024-06-05 09:40:38 -0300

    Remove extra files

--- pthread/scriptXeon_cmpPthreadV2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH --partition=XeonPHI
-#SBATCH -o Resultados/output.txt
-#SBATCH -e Resultados/errors.txt
-source $ONEAPI_PATH/setvars.sh > /dev/null 2>&1
-icc -pthread -o cmpAB_pthread_tempAB cmpAB_pthread_tempAB.c
-./cmpAB_pthread_tempAB $1 $2
